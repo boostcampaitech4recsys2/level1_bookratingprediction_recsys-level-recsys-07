85c66f14bccc848c367a0fcbc6697f700ec651a3
JM KIM <kimjinm77@gmail.com>
2022-11-01 03:05:05 +0000

    language& book_title+summary clustering

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-python3 main.py --MODEL CNN_FM --EPOCH 10 #--LR 1e-4 #--DEEPCONN_VECTOR_CREATE True
+python3 main.py --MODEL FM --EPOCH 10 #--LR 1e-4 #--DEEPCONN_VECTOR_CREATE True
 # FM FFM NCF WDN DCN CNN_FM DeepCoNN
 
 # efficient-net
