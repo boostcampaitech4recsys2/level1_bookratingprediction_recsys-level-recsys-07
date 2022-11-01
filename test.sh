a8f9a6946e5b7dd4377878fc85dc6c33f81ef2d0
msk <kms950216@gmail.com>
2022-11-01 05:02:11 +0000

    add users_preprocessed.csv

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 # DeepFM
-python main.py --MODEL DFM --DFM_EMBED_DIM 5 --DFM_DROPOUT 0.2 --DFM_USE_BN True --EPOCHS 1 --MESSAGE embed_dim_5_dropout_0.2_use_bn_True_preprocessed
-# python main.py --MODEL FM --FM_EMBED_DIM 4 --MESSAGE not_impute_age
+# python main.py --MODEL DFM --DFM_EMBED_DIM 5 --DFM_DROPOUT 0.2 --DFM_USE_BN True --EPOCHS 1 --MESSAGE embed_dim_5_dropout_0.2_use_bn_True_preprocessed
+python main.py --MODEL FM --FM_EMBED_DIM 4 --MESSAGE modified
