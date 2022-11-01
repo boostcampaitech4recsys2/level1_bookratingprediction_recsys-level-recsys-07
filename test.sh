0951cab00151100ba34512e8bf01cfd30c5c3fa9
msk <kms950216@gmail.com>
2022-11-01 07:56:55 +0000

    tmp

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 # DeepFM
-# python main.py --MODEL DFM --DFM_EMBED_DIM 5 --DFM_DROPOUT 0.2 --DFM_USE_BN True --EPOCHS 1 --MESSAGE embed_dim_5_dropout_0.2_use_bn_True_preprocessed
-python main.py --MODEL FM --FM_EMBED_DIM 4 --MESSAGE modified_drop_city
+python main.py --MODEL DFM --DFM_EMBED_DIM 5 --DFM_DROPOUT 0.2 --DFM_USE_BN True --EPOCHS 10 --MESSAGE embed_dim_5_dropout_0.2_use_bn_True_epochs_10_preprocessed_modified
+# python main.py --MODEL FM --FM_EMBED_DIM 4 --MESSAGE modified_drop_city
